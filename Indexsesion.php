03589417f123670fb9dcb62f566e26bca90199fe
Estefania <estefaniajimenezpana@gmail.com>
2023-08-08 16:54:36 -0500

    ejercicio

---
+++ Indexsesion.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bievenido</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <div class="container">
+<h2>BIEVENIDO!</h2></body>
+<p>Has iniciado sesión con éxito.</p>
+<a href="Index.php">Cerrar sesión</a>
+</div>
+</body>
+</html>
